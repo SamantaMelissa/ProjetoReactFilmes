62a6e2743876c52d2d5174cbfa9d1862ade58c34
Samanta Melissa <samanta.melissa13@gmail.com>
2025-04-24 17:31:42 -0300

    Tela de cadastro de filmes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import CadastroFilme from './pages/cadastroFilme/CadastroFilme.jsx';
 function App() {
   return (
     <>
-    {/* chamar as páginas */}
+      {/* chamar as páginas */}
       {/* <Login/> */}
       <CadastroFilme/>
     </>
---
+++ src/components/lista/Lista.jsx
@@ -0,0 +1,40 @@
+import "./Lista.css";
+
+// Importação de imagens:
+import Editar from "../../assets/img/pen-to-square-solid.svg";
+import Excluir from "../../assets/img/trash-can-regular.svg";
+
+const Lista = () => {
+    return(
+        <section className="layout_grid listagem">
+            <h1>Lista dos Filmes</h1>
+            <hr/>
+            <div className="tabela">
+                <table>
+                    {/* cabeçalho da tabela: */}
+                    <thead>
+                        {/* tr => table row */}
+                        <tr className="cabecalho">
+                            {/* th => table head */}
+                            <th>Nome</th>
+                            <th>Gênero</th>
+                            <th>Editar</th>
+                            <th>Excluir</th>
+                        </tr>
+                    </thead>
+                    {/* tbody => corpo da tabela */}
+                    <tbody>
+                        <tr className="item_lista">
+                            <td  data-cell="Nome" >Velozes e furiosos</td>
+                            <td  data-cell="Gênero" >Ação</td>
+                            <td><img src={Editar} alt="Caneta"/></td>
+                            <td><img src={Excluir} alt="Lixeira"/></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </section>
+    )
+}
+
+export default Lista;
